b17d45bf134b26f05b4e061863a772dfc878101b
noramibu <50046813+noramibu@users.noreply.github.com>
2026-07-03 04:50:31 +0300

    release 1.0.3 - provided gradle wrapper - added DISABLE_COMMANDS_IN_PVP gamerule, false as default - added TAG_ATTACKERS gamerule, false as default - bumped mc_version to 26.2.x

--- src/main/java/com/github/tatercertified/vanilla/mixin/CombatTrackerMixin.java
+++ src/main/java/com/github/tatercertified/vanilla/mixin/CombatTrackerMixin.java
@@ -6,7 +6,6 @@ package com.github.tatercertified.vanilla.mixin;
 
 import com.github.tatercertified.vanilla.CombatLogger;
 import com.github.tatercertified.vanilla.FairFight;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.CombatTracker;
@@ -62,8 +61,16 @@ public class CombatTrackerMixin implements CombatLogger {
             method = "recordDamage",
             at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
     private void fairfight$setLastDamageTime(DamageSource source, float damage, CallbackInfo ci) {
-        if (source.getEntity() instanceof Player) {
+        if (source.getEntity() instanceof Player attacker) {
             lastPlayerDamageTime = this.mob.tickCount;
+            if (this.mob instanceof Player
+                    && attacker != this.mob
+                    && !this.mob.level().isClientSide()
+                    && ((ServerLevel) this.mob.level())
+                            .getGameRules()
+                            .get(FairFight.TAG_ATTACKERS)) {
+                ((CombatLogger) attacker.getCombatTracker()).markPlayerCombat();
+            }
         }
     }
 
@@ -76,8 +83,8 @@ public class CombatTrackerMixin implements CombatLogger {
                                     "Lnet/minecraft/world/damagesource/CombatTracker;takingDamage:Z",
                             ordinal = 0,
                             opcode = Opcodes.GETFIELD))
-    private void fairfight$recheckStatus(CallbackInfo ci, @Local(ordinal = 0) int i) {
-        if (this.inPlayerCombat && this.mob.tickCount - this.lastPlayerDamageTime > i) {
+    private void fairfight$recheckStatus(CallbackInfo ci) {
+        if (this.inPlayerCombat && getPlayerCombatSecondsLeft() <= 0) {
             this.inPlayerCombat = false;
         }
     }
@@ -116,6 +123,12 @@ public class CombatTrackerMixin implements CombatLogger {
                                     - combatDuration)
                             / 20.0F);
         }
+    }
+
+    @Override
+    public void markPlayerCombat() {
+        this.inPlayerCombat = true;
+        this.lastPlayerDamageTime = this.mob.tickCount;
     }
 
     @Override
--- src/main/java/com/github/tatercertified/vanilla/mixin/ServerGamePacketListenerMixin.java
+++ src/main/java/com/github/tatercertified/vanilla/mixin/ServerGamePacketListenerMixin.java
@@ -15,10 +15,22 @@ import net.minecraft.server.players.PlayerList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerGamePacketListenerImpl.class)
 public class ServerGamePacketListenerMixin {
     @Shadow public ServerPlayer player;
+
+    @Inject(method = {"handleChatCommand", "handleSignedChatCommand"}, at = @At("HEAD"), cancellable = true)
+    private void fairfight$blockCommandsInPvp(CallbackInfo ci) {
+        if (this.player.level().getGameRules().get(FairFight.DISABLE_COMMANDS_IN_PVP)
+                && ((CombatLogger) this.player.getCombatTracker()).isInPlayerCombat()) {
+            this.player.sendSystemMessage(
+                    Component.literal("You cannot use commands in PvP combat."));
+            ci.cancel();
+        }
+    }
 
     @WrapOperation(
             method = "removePlayerFromWorld",
